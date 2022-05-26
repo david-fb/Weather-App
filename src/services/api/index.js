15f2184d83a10003ecb6580d7645f787c595b902
David Basto <davidbasto01@gmail.com>
2022-05-25 19:14:38 -0500

    [ADD] search location

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,7 +2,8 @@ const API = process.env.NEXT_PUBLIC_API_URL;
 
 const endPoints = {
   realtimeWeatherFrom: (query) => `${API}/current.json?q=${query}`,
-  forecastWeatherFrom: (query) => `${API}/forecast.json?q=${query}`,
+  forecastWeatherFrom: (query, days = 3) => `${API}/forecast.json?q=${query}&days=${days}`,
+  searchLocation: (query) => `${API}/search.json?q=${query}`,
 };
 
 module.exports = endPoints;
--- src/services/api/weather.js
+++ src/services/api/weather.js
@@ -10,32 +10,47 @@ const options = {
 
 const getWeatherFrom = async (query) => {
   try {
-    const response = await fetch(endPoints.realtimeWeatherFrom(query), options);
+    const response = await fetch(endPoints.forecastWeatherFrom(query), options);
     const data = await response.json();
 
-    const { location, current } = data;
+    const { location, current, forecast } = data;
     const { country, localtime, name } = location;
     const { condition, humidity, feelslike_c, feelsLike_f, is_day, temp_c, temp_f, wind_mph, wind_kph, wind_dir } = current;
+    const { forecastday } = forecast;
     const { text, icon } = condition;
 
     const body = {
-      conditionText: text,
-      conditionIcon: icon,
-      country,
-      localtime,
-      locationName: name,
-      humidity,
-      isDay: is_day,
-      feelsLike: { C: feelslike_c, F: feelsLike_f },
-      temperature: { C: temp_c, F: temp_f },
-      windSpeed: { K: wind_kph, M: wind_mph },
-      windDir: wind_dir,
+      current: {
+        conditionText: text,
+        conditionIcon: icon,
+        country,
+        localtime,
+        locationName: name,
+        humidity,
+        isDay: is_day,
+        feelsLike: { C: feelslike_c, F: feelsLike_f },
+        temperature: { C: temp_c, F: temp_f },
+        windSpeed: { K: wind_kph, M: wind_mph },
+        windDir: wind_dir,
+      },
+      forecast: { ...forecastday },
     };
 
-    return body;
+    return { ...body };
   } catch (error) {
     console.error(error);
   }
 };
 
-module.exports = { getWeatherFrom };
+const searchLocation = async (query) => {
+  try {
+    const response = await fetch(endPoints.searchLocation(query), options);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { getWeatherFrom, searchLocation };
