1dcab45ec9d62c9ce08ac728babdd3703e774a38
David Basto <davidbasto01@gmail.com>
2022-05-25 19:09:38 -0500

    [ADD] weather state

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,28 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 let initialState = {
   temperature: 'C',
-  distance: 'K',
+  speed: 'K',
   location: '',
 };
 
 const useInitialState = () => {
   const [preferences, setPreferences] = useState(initialState);
+  const [weather, setWeather] = useState({});
 
   const setTemperature = (payload) => {
     setPreferences({ ...preferences, temperature: payload });
     localStorage.setItem('temperature', payload);
   };
 
-  const setDistance = (payload) => {
-    setPreferences({ ...preferences, distance: payload });
-    localStorage.setItem('distance', payload);
+  const setSpeed = (payload) => {
+    setPreferences({ ...preferences, speed: payload });
+    localStorage.setItem('speed', payload);
   };
 
   const setLocation = (payload) => {
     setPreferences({ ...preferences, location: payload });
   };
 
-  return { preferences, setTemperature, setDistance, setLocation };
+  useEffect(() => {
+    const temperatureLocal = localStorage.getItem('temperature');
+    const speedLocal = localStorage.getItem('speed');
+    if (temperatureLocal) {
+      setPreferences((state) => {
+        return {
+          ...state,
+          temperature: temperatureLocal,
+        };
+      });
+    }
+    if (speedLocal) {
+      setPreferences((state) => {
+        return { ...state, speed: speedLocal };
+      });
+    }
+  }, []);
+
+  return { preferences, setTemperature, setSpeed, setLocation, weather, setWeather };
 };
 
 export default useInitialState;
