d00de4e531343db20aed62f53a2c57a8287afe28
David Basto <davidbasto01@gmail.com>
2022-06-01 14:11:27 -0500

    [FIX] responsive

--- src/components/CityWeather.jsx
+++ src/components/CityWeather.jsx
@@ -14,7 +14,10 @@ export default function CityWeather({ city }) {
         <p className={styles['container__location__time']}>{city.localtime}</p>
       </div>
       <div className={styles['container__temperature']}>
-        <p className={styles['container__temperature__text']}>{city?.current.temperature?.C}&#xb0;</p>
+        <p className={styles['container__temperature__text']}>
+          {city?.current.temperature?.C}
+          <span>&#xb0;</span>
+        </p>
         <button onClick={() => removeCity(city.url)} className={styles['container__temperature__remove']}>
           X
         </button>
