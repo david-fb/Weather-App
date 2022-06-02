f5235cea1cb547b3c68d2fcb068d429e687b4588
David Basto <davidbasto01@gmail.com>
2022-06-01 23:34:29 -0500

    [UPDATE] head title and favicon

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,7 +1,11 @@
 import Navbar from './Navbar';
+import Head from 'next/head';
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Weather App - Midudev</title>
+      </Head>
       {children}
       <Navbar />
     </>
